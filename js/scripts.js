8143ce34786070ecd66ca1f56607938ceaf220cc
Derek Villars <DerekVillars@yahoo.com>
2017-01-20 09:34:31 -0800

    fix README.md to match what i want in the specs. and add to if to make 5s Zag

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ function zigZag(textInput) {
   var numberInput = parseInt(textInput);
   var arrayOutput = []
   for (var i = 1; i <= numberInput; i++) {
-    if(i % 3 === 0) {
+    if (i % 5 === 0) {
+      arrayOutput.push('Zig');
+    } else if (i % 3 === 0) {
       arrayOutput.push('Zag');
     } else {
       arrayOutput.push(i);
